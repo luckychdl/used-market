1c9c16ffdedf8e157ceb4afe5180168dac0b7e62
luckychdl <luckysksk@naver.com>
2021-11-29 18:01:25 +0900

    update

--- pages/usedMarket/detail/[useditemId]/index.tsx
+++ pages/usedMarket/detail/[useditemId]/index.tsx
@@ -1,29 +1,29 @@
 import MarketDetail from "../../../../src/components/units/usedMarket/detail/marketDetail.container";
 import MarketCommentWrite from "../../../../src/components/units/marketComment/write/marketCommentWrite.container";
 import MarketCommentList from "../../../../src/components/units/marketComment/list/marketCommentList.container";
-// import { request, gql } from "graphql-request";
-// import Head from "next/head";
-// const FETCH_USEDITEM = gql`
-//   query fetchUseditem($useditemId: ID!) {
-//     fetchUseditem(useditemId: $useditemId) {
-//       name
-//       remarks
-//       images
-//     }
-//   }
-// `;
+import { request, gql } from "graphql-request";
+import Head from "next/head";
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
 
 const MarketDetailPage = (props: any) => {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:title" content={props.fetchUseditem.name} />
         <meta property="og:description" content={props.fetchUseditem.remarks} />
         <meta
           property="og:image"
           content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
         />
-      </Head> */}
+      </Head>
       <MarketDetail />
       <MarketCommentWrite />
       <MarketCommentList />
@@ -33,13 +33,13 @@ const MarketDetailPage = (props: any) => {
 
 export default MarketDetailPage;
 
-// export const getServerSideProps = async (context: any) => {
-//   const result = await request(
-//     "https://backend02.codebootcamp.co.kr/graphql07",
-//     FETCH_USEDITEM,
-//     {
-//       useditemId: context.query.useditemId,
-//     }
-//   );
-//   return { props: { fetchUseditem: result.fetchUseditem } };
-// };
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql07",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
+  return { props: { fetchUseditem: result.fetchUseditem } };
+};
